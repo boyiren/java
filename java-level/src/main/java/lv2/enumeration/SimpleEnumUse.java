76ee0b821c20e0146866575e859716912906ef2c
boyiren <17825270377@163.com>
2019-10-02 11:30:51 +0800

    学习thinking in java 注解章节，进度：0.2

---
+++ java-level/src/main/java/lv2/enumeration/SimpleEnumUse.java
@@ -0,0 +1,23 @@
+package lv2.enumeration;
+
+/**
+ * @author boyiren
+ * @date 2019-10-02
+ */
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+
+        System.out.println(howHot);
+        System.out.println(howHot.name());
+        System.out.println(howHot.hashCode());
+        System.out.println(howHot.toString());
+        System.out.println(howHot.getClass());
+        System.out.println(howHot.getDeclaringClass());
+        System.out.println(howHot.ordinal());
+
+        for (Spiciness spiciness : Spiciness.values()) {
+            System.out.println(spiciness + ".ordinal" + spiciness.ordinal());
+        }
+    }
+}
---
+++ java-level/src/main/java/lv2/enumeration/Spiciness.java
@@ -0,0 +1,30 @@
+package lv2.enumeration;
+
+/**
+ * 辛辣程度 枚举
+ *
+ * @author boyiren
+ * @date 2019-10-02
+ */
+public enum Spiciness {
+    /**
+     * 不辣
+     */
+    NOT,
+    /**
+     * 微辣
+     */
+    MILD,
+    /**
+     * 中辣
+     */
+    MEDIUM,
+    /**
+     * 重辣
+     */
+    HOT,
+    /**
+     * 燃烧我的卡路里。。
+     */
+    FLAMING
+}
